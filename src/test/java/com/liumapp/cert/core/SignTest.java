619704c1457098ebbfc41eb3d816945c7e5fe08e
liumapp <liumapp.com@gmail.com>
2018-04-11 17:30:40 +0800

    success !

--- src/test/java/com/liumapp/cert/core/SignTest.java
+++ src/test/java/com/liumapp/cert/core/SignTest.java
@@ -36,7 +36,7 @@ public class SignTest extends TestCase {
 
             KeyStore ks = KeyStore.getInstance("JKS");
             ks.load(new FileInputStream(pdfSignByAliasPattern.getKeystore()) , pdfSignByAliasPattern.getKeystorePd().toCharArray());
-//            KeyStore.Entry entry = ks.getEntry(pdfSignByAliasPattern.getAlias() , new KeyStore.PasswordProtection(null));
+            KeyStore.Entry entry = ks.getEntry(pdfSignByAliasPattern.getAlias() , new KeyStore.PasswordProtection(pdfSignByAliasPattern.getCertPd().toCharArray()));
             Certificate certificate = ks.getCertificate(pdfSignByAliasPattern.getAlias());
             Certificate[] chain = ks.getCertificateChain(pdfSignByAliasPattern.getAlias());
 
@@ -65,12 +65,12 @@ public class SignTest extends TestCase {
 
     private PdfSignByAliasPattern initPdfSignByAliasPattern () {
         PdfSignByAliasPattern pdfSignByAliasPattern = new PdfSignByAliasPattern();
-//        pdfSignByAliasPattern.setAlias("958c82d956b4bf5229ec59e9e58c113ed877e88c");
-        pdfSignByAliasPattern.setAlias("19643137d8482943fbdd94075cd4302edd5b0908");
+        pdfSignByAliasPattern.setAlias("958c82d956b4bf5229ec59e9e58c113ed877e88c");
+//        pdfSignByAliasPattern.setAlias("19643137d8482943fbdd94075cd4302edd5b0908");
 //        pdfSignByAliasPattern.setAlias("first-certificate");
         pdfSignByAliasPattern.setKeystore("/usr/local/tomcat/project/sign-core/ks/demo.ks");
         pdfSignByAliasPattern.setKeystorePd("123456");
-        pdfSignByAliasPattern.setCertPd("1");
+        pdfSignByAliasPattern.setCertPd("123456");
         pdfSignByAliasPattern.setSignatureField("certA");
         pdfSignByAliasPattern.setTmpImg("/usr/local/tomcat/project/sign-core/pic/pic.jpg");
         pdfSignByAliasPattern.setTmpPdf("/usr/local/tomcat/project/sign-core/pdf/with_sign_area_test.pdf");
