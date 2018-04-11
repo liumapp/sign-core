1d85e909072c4a0b8bb913d12a5eaa327329d9ab
liumapp <liumapp.com@gmail.com>
2018-04-11 14:21:39 +0800

    ready to test

--- src/test/java/com/liumapp/cert/core/SignTest.java
+++ src/test/java/com/liumapp/cert/core/SignTest.java
@@ -2,15 +2,9 @@ package com.liumapp.cert.core;
 
 import com.itextpdf.text.pdf.PdfSignatureAppearance;
 import com.itextpdf.text.pdf.security.DigestAlgorithms;
-import com.liumapp.cert.core.constant.SignConstant;
+import com.liumapp.cert.core.entity.Sign;
+import com.liumapp.cert.core.entity.SignatureInfo;
 import com.liumapp.cert.core.pattern.PdfSignByAliasPattern;
-import com.liumapp.cert.core.property.Properties;
-import com.liumapp.sign.core.constant.SignConstant;
-import com.liumapp.sign.core.pattern.PdfSignByAliasPattern;
-import com.liumapp.sign.core.pattern.PdfSignBySerialNumPattern;
-import com.liumapp.sign.core.pdf.Sign;
-import com.liumapp.sign.core.pdf.SignatureInfo;
-import com.liumapp.sign.core.property.Properties;
 import junit.framework.TestCase;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -35,12 +29,10 @@ public class SignTest extends TestCase {
         PdfSignByAliasPattern pdfSignByAliasPattern = initPdfSignByAliasPattern();
 
         try {
-            String tmpPdfOut = pdfSignByAliasPattern.getTmpPdf() + "final";
+            String tmpPdfOut = "final_" + pdfSignByAliasPattern.getTmpPdf() ;
 
             KeyStore ks = KeyStore.getInstance("jks");
-            ks.load(new FileInputStream(properties.getString(SignConstant.save_KeyStorePath)
-                    + pdfSignByAliasPattern.getKeystore()) ,
-                    properties.getString(SignConstant.keyStore_password).toCharArray());
+            ks.load(new FileInputStream(pdfSignByAliasPattern.getKeystore()) , pdfSignByAliasPattern.getKeystorePd().toCharArray());
 
             PrivateKey pk = (PrivateKey) ks.getKey(pdfSignByAliasPattern.getAlias() , pdfSignByAliasPattern.getCertPd().toCharArray());
             Certificate[] chain = ks.getCertificateChain(pdfSignByAliasPattern.getAlias());
@@ -51,13 +43,13 @@ public class SignTest extends TestCase {
             signatureInfo.setPk(pk);
             signatureInfo.setChain(chain);
             signatureInfo.setCertificationLevel(PdfSignatureAppearance.NOT_CERTIFIED);
-            signatureInfo.setDigestAlgorithm(DigestAlgorithms.SHA256);
+            signatureInfo.setDigestAlgorithm(DigestAlgorithms.SHA1);
             signatureInfo.setFieldName(pdfSignByAliasPattern.getSignatureField());
-            signatureInfo.setImagePath(properties.getString(SignConstant.save_PicPath) + pdfSignByAliasPattern.getTmpImg());
+            signatureInfo.setImagePath(pdfSignByAliasPattern.getTmpImg());
             signatureInfo.setRenderingMode(PdfSignatureAppearance.RenderingMode.GRAPHIC);
 
-            Sign.sign(properties.getString(SignConstant.save_PdfPath) + pdfSignByAliasPattern.getTmpPdf() ,
-                    properties.getString(SignConstant.save_PdfPath) + tmpPdfOut , signatureInfo);
+            Sign.sign(pdfSignByAliasPattern.getTmpPdf() ,
+                    tmpPdfOut , signatureInfo);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -65,27 +57,16 @@ public class SignTest extends TestCase {
 
     }
 
-    private PdfSignBySerialNumPattern initPdfSignBySerialNumPattern () {
-        PdfSignBySerialNumPattern pdfSignBySerialNumPattern = new PdfSignBySerialNumPattern();
-        pdfSignBySerialNumPattern.setSerialNum("2c165439cd0bb006baa1ed28715af2e3f1a7eb25");
-        pdfSignBySerialNumPattern.setKeystore("demo.ks");
-        pdfSignBySerialNumPattern.setCertPd("password");
-        pdfSignBySerialNumPattern.setSignatureField("certA");
-        pdfSignBySerialNumPattern.setTmpImg("pic.jpg");
-        pdfSignBySerialNumPattern.setTmpPdf("with_sign_area_test.pdf");
-        return pdfSignBySerialNumPattern;
-    }
-
     private PdfSignByAliasPattern initPdfSignByAliasPattern () {
         PdfSignByAliasPattern pdfSignByAliasPattern = new PdfSignByAliasPattern();
 //        pdfSignByAliasPattern.setAlias("4bbdc076d4493b52209b17643fb85b00baa58522");
         pdfSignByAliasPattern.setAlias("first-certificate");
-        pdfSignByAliasPattern.setKeystore("demo.ks");
+        pdfSignByAliasPattern.setKeystore("/usr/local/tomcat/project/sign-core/ks/demo.ks");
         pdfSignByAliasPattern.setKeystorePd("123456");
         pdfSignByAliasPattern.setCertPd("123123");
         pdfSignByAliasPattern.setSignatureField("certA");
-        pdfSignByAliasPattern.setTmpImg("pic.jpg");
-        pdfSignByAliasPattern.setTmpPdf("with_sign_area_test.pdf");
+        pdfSignByAliasPattern.setTmpImg("/usr/local/tomcat/project/sign-core/pic/pic.jpg");
+        pdfSignByAliasPattern.setTmpPdf("/usr/local/tomcat/project/sign-core/pdf/with_sign_area_test.pdf");
         return pdfSignByAliasPattern;
     }
 
